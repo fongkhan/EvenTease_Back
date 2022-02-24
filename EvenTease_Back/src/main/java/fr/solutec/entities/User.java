e01323e30e1a42fa3a452ff31e3c7b2ea8317b17
Youssef <youssefbaissi@gmail.com>
2022-02-24 17:31:43 +0100

    Youssef's version

--- EvenTease_Back/src/main/java/fr/solutec/entities/User.java
+++ EvenTease_Back/src/main/java/fr/solutec/entities/User.java
@@ -24,7 +24,7 @@ public class User {
 	private String password;
     
     @Column(unique=true, nullable = false)
-	private String mail;
+	public String mail;
     @Column(nullable=false)
 	private String surname;
     @Column(nullable=false)
