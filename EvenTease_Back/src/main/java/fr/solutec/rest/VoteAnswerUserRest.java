6e65ad0cf52510a331e7f3388e9ba6e846a319dd
fongkhan <fong.vu@hotmail.fr>
2022-02-28 22:02:39 +0100

    ajout système de vote

---
+++ EvenTease_Back/src/main/java/fr/solutec/rest/VoteAnswerUserRest.java
@@ -0,0 +1,28 @@
+package fr.solutec.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import fr.solutec.entities.User;
+import fr.solutec.entities.VoteAnswerUser;
+import fr.solutec.repository.VoteAnswerUserRepository;
+
+@RestController @CrossOrigin("*")
+public class VoteAnswerUserRest {
+	@Autowired
+	private VoteAnswerUserRepository vauRepo;
+	
+	@GetMapping("event/vote/answer/user")
+	public Iterable<VoteAnswerUser> allVoteAnswerUser(){
+		return vauRepo.findAll();
+	}
+	
+	@GetMapping("event/vote/answer/user/add/{id}")
+	public VoteAnswerUser addVoteAnswerUser(@PathVariable Long id, @RequestBody User user) {
+		return null;
+	}
+}
