43c89e51f03ca0618ac15f3a67d7f8d28ff63070
Youssef <youssefbaissi@gmail.com>
2022-03-04 12:11:48 +0100

    Ajout API shoppinglist

--- EvenTease_Back/src/main/java/fr/solutec/repository/ShoppingListRepository.java
+++ EvenTease_Back/src/main/java/fr/solutec/repository/ShoppingListRepository.java
@@ -1,9 +1,13 @@
 package fr.solutec.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
+import fr.solutec.entities.Event;
 import fr.solutec.entities.ShoppingList;
 
 public interface ShoppingListRepository extends CrudRepository<ShoppingList, Long>{
+	public Iterable<ShoppingList> findByEventId(Long id);
 
 }
--- EvenTease_Back/src/main/java/fr/solutec/rest/ShoppingListRest.java
+++ EvenTease_Back/src/main/java/fr/solutec/rest/ShoppingListRest.java
@@ -3,6 +3,7 @@ package fr.solutec.rest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,11 @@ public class ShoppingListRest {
 	@GetMapping("event/shoppingList")
 	public Iterable<ShoppingList> getAllShoppingList(){
 		return shopRepo.findAll();
+	}
+	
+	@GetMapping("event/shoppingList/{idEvent}")
+	public Iterable<ShoppingList> getAllShoppingList(@PathVariable Long idEvent){
+		return shopRepo.findByEventId(idEvent);
 	}
 	
 	@PostMapping("shoppinglist/add")
